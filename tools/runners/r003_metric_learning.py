33cd2f0236aef62f8062dca9fc6b8e901f4f99a8
guchio3 <astt.hwhw@gmail.com>
2019-09-13 05:46:30 +0000

    tmep

--- tools/runners/r003_metric_learning.py
+++ tools/runners/r003_metric_learning.py
@@ -394,7 +394,7 @@ class Runner(object):
         if epoch == 1:
             # for name, child in self.model.named_children():
             for name, child in self.model.module.named_children():
-                if 'fc' in name or 'arc' in name:
+                if 'fc' in name or 'arc' in name or '_conv_stem' in name or 'bn00' in name:
                     sel_log(name + ' is unfrozen', self.logger)
                     for param in child.parameters():
                         param.requires_grad = True
