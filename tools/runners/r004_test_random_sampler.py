399aef97f2ad9b02f4287b283b8f6e34408a9d8c
guchio3 <astt.hwhw@gmail.com>
2019-09-20 04:19:10 +0000

    temp

--- tools/runners/r004_test_random_sampler.py
+++ tools/runners/r004_test_random_sampler.py
@@ -26,6 +26,7 @@ from ..schedulers import CosineAnnealingWarmUpRestarts as cawur
 from ..schedulers import pass_scheduler
 from ..utils.logs import sel_log, send_line_notification
 from ..utils.splittings import CellwiseStratifiedKFold as cskf
+from ..utils.splittings import ExperimentwiseSplit as ews
 
 
 class Runner(object):
@@ -213,8 +214,8 @@ class Runner(object):
             dataset,
             batch_size=batch_size,
             sampler=sampler,
-            # num_workers=cpu_count(),
-            num_workers=0,
+            num_workers=os.cpu_count(),
+            # num_workers=0,
             worker_init_fn=lambda x: np.random.seed(),
             drop_last=drop_last,
             pin_memory=True,
@@ -419,6 +420,13 @@ class Runner(object):
                 .split(trn_df['id_code'], trn_df['sirna'])
         elif split_type == 'cskf':
             fold = cskf(
+                trn_df,
+                trn_df['sirna'],
+                split_num,
+                shuffle=True,
+                random_state=71)
+        elif split_type == 'ews':
+            fold = ews(
                 trn_df,
                 trn_df['sirna'],
                 split_num,
